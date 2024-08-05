a4ee6aeb02d9fc1f4b6849926b922e4ab6b4f31b
Ирина Кривошея <ma_irin@bk.ru>
2024-08-05 03:14:33 +0300

    modify:processing.py,conftest.py,test_processing.py

--- src/processing.py
+++ src/processing.py
@@ -12,14 +12,14 @@ def sort_by_date(spis_input: list[dict], reverse_input: bool = True) -> list[dic
     return sort_spis
 
 
-if __name__ == "__main__":
-    spis_bank_operation = [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
-
-    print(filter_by_state(spis_bank_operation))
-
-    print(sort_by_date(spis_bank_operation))
+# if __name__ == "__main__":
+#     spis_bank_operation = [
+#         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+#     ]
+#
+#     print(filter_by_state(spis_bank_operation))
+#
+#     print(sort_by_date(spis_bank_operation))
