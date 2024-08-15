16d6cf242e0b6064684ccb44ecce1a03194bfcbc
Ирина Кривошея <ma_irin@bk.ru>
2024-08-15 08:49:57 +0300

    modify:test_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,21 +1,18 @@
 import pytest
-
+from pathlib import Path
+from time import time
+from typing import Any, Callable
 from src.decorators import decorator_log
-
 from src.decorators import example
 
 def test_decorator_log(capsys):
-    decorator_log()
+    decorator_log(function=example)
     captured = capsys.readouterr()
-    assert captured.out == f"Вызвана функция: example"
+    assert captured.out == ''
 
 
-def test_decorator_log(predical_file_print):
-    with pytest.raises(ValueError, predical_file_print):
-        example('../Dat/log.txt')
-
-
-def test_example(file_input):
-    with pytest.raises(OSError, file_input, error_message='Файл для вывода данных'):
-        example('../Dat/log.txt')
+# def test_example(path_input: str) -> Any:
+#     if Path(path_input).exists is False:
+#         raise ValueError('директория для выходного файла не найдена')
+#     return path_input
 
