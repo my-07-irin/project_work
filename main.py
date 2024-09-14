f6394ad5dd7707cf298d684f97f97fc670ea9ffd
Ирина Кривошея <ma_irin@bk.ru>
2024-09-14 12:30:57 +0300

    modify:README.md, main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from src.masks import get_mask_account, get_mask_card_number
 from src.utils import read_file
 from src.category import Category
 from src.product import Product
@@ -6,11 +5,7 @@ from src.read_from_file_product import open_read_file, create_object_category_pr
 
 
 def main() -> None:
-    '''логирование приложений'''
-    read_file("data/operations.json")
-    get_mask_card_number("1234561234567845")
-    get_mask_account("12345678901234567890")
-
+    '''создание объектов категории продуктов и списка продуктов'''
     create_object_category_product(open_read_file("/data/products.json"))
     return
 
